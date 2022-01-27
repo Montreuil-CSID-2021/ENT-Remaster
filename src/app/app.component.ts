6354d9416a9ffbed5ec80f1f83d5a2445c51ce0e
Smyllet <smyllet.gamer@gmail.com>
2022-01-27 09:08:10 +0100

    Revert "Revert "bricolage""
    This reverts commit 9d6b4e73a8d932e08755ce99c59c802e2c89a1e5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+import * as config from "../../config.json";
+
+export interface apodPicture {
+  hdurl: string
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _httpClient: HttpClient) {
+  }
+
   title = 'nasangularproject';
+  homePageImage = "https://http.cat/102";
+
+  ngOnInit() {
+    this._httpClient.get<apodPicture>(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .subscribe(res => {
+        this.homePageImage = res.hdurl
+      })
+  }
 }
