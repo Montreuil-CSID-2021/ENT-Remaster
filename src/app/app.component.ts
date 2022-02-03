4e19a9f7c9664a718ab7ceee65554ae4495cef1b
Smyllet <smyllet.gamer@gmail.com>
2022-02-03 16:54:35 +0100

    [ADD] ENT event in syncfusion calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,53 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {
-  DayService,
-  WeekService,
-  WorkWeekService,
-  MonthService,
   AgendaService,
-  EventSettingsModel
-} from '@syncfusion/ej2-angular-schedule';
+  DayService,
+  EventSettingsModel,
+  MonthService,
+  ScheduleComponent,
+  WeekService
+} from "@syncfusion/ej2-angular-schedule";
 import {EDTApi} from "./edt/EDTApi";
-import config from "../../config.json";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService]
+  providers: [DayService, WeekService, MonthService, AgendaService]
 })
 
 export class AppComponent implements OnInit {
+  @ViewChild('scheduleObj') scheduleObj!: ScheduleComponent;
+
   private edtApi: EDTApi
   constructor(private _httpClient: HttpClient) {
     this.edtApi = new EDTApi(_httpClient)
   }
 
-  public data: object[] = [
-    {
-      Subject: 'Meeting',
-      StartTime: new Date(2022, 1, 3, 10, 0),
-      EndTime: new Date(2022, 1, 3, 12, 0)
-    }
-  ]
-
   public eventSettings: EventSettingsModel = {
     allowEditing: false,
-    dataSource: this.data,
     allowAdding: false,
     allowDeleting: false,
   }
 
   title = 'Emploi du temps CSID';
 
+
   ngOnInit() {
     console.log("Initialisation")
-    this.edtApi.getEdt().then((edt) => {
+    this.edtApi.getEdt().then(async (edt) => {
       console.log(edt)
-      this.data = this.data.concat(edt.map(e => {
-
-      }))
+      this.scheduleObj.eventSettings.dataSource = edt.map(e => {
+        let startDate = new Date(e.debut * 1000)
+        let endDate = new Date(e.fin * 1000)
+        return {
+          Subject: e.mat,
+          StartTime: startDate,
+          EndTime: endDate,
+          Location: `${e.salle} - ${e.prof}`,
+        }
+      })
     })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
 import { AppComponent } from './app.component';
-//import { ScheduleComp } from './schedule.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    //ScheduleComp
+    AppComponent
   ],
   imports: [
     BrowserModule,
